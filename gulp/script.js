570abe8ca09751f02cefccd2090e6a33660c1f9b
yehuiwei <yehuiwei@hengtiansoft.com>
2017-10-16 16:52:02 +0800

    add gulp-browserify to translate require()

--- gulp/script.js
+++ gulp/script.js
@@ -3,11 +3,14 @@ const gulp = require('gulp');
 const conf = require('./conf');
 const babel = require('gulp-babel');
 
+const $ = require('gulp-load-plugins')();
+
 const sources = [ path.join(conf.paths.src, 'app/index.module.js') ];
 
 function buildScripts() {
     return gulp.src(sources)
         .pipe(babel())
+        .pipe($.browserify())
         .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app')));
 }
 
