2505c0de1dbd05782694a4744bee9d7cf73fed23
yehuiwei <yehuiwei@hengtiansoft.com>
2017-10-16 17:09:56 +0800

    add modal

---
+++ src/app/components/modal/modal.component.js
@@ -0,0 +1,15 @@
+export function ModalComponent() {
+    let component = {
+        templateUrl: '',
+        controller: ModalController,
+        controllerAs: 'vm'
+    };
+
+    return component;
+
+    class ModalController {
+        constructor () {
+
+        }
+    }
+}
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,3 +1,5 @@
 import angular from 'angular';
+import { ModalComponent } from "./components/modal/modal.component";
 
-angular.module('ng1gulp', []);
+angular.module('ng1gulp', [])
+    .component('Modal', ModalComponent);
