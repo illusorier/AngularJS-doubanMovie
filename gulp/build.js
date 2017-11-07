c8d0f394e601bf44dd98ccc2efd762486c991913
yehuiwei <yehuiwei@hengtiansoft.com>
2017-11-07 15:18:27 +0800

    enable using templateUrl

--- gulp/build.js
+++ gulp/build.js
@@ -3,8 +3,32 @@ const conf = require('./conf');
 const path = require('path');
 const del = require('del');
 
-gulp.task('clean', function () {
+const $ = require('gulp-load-plugins')();
+
+gulp.task('partials', () => {
+    return gulp.src([path.join(conf.paths.src, '/app/**/*.html')])
+        .pipe($.angularTemplatecache('templateCacheHtml.js', {
+            module: 'ng1Dashboard',
+            root: 'app'
+        }))
+        .pipe(gulp.dest(conf.paths.tmp + '/serve/app'));
+});
+
+gulp.task('html', ['inject', 'partials'], function () {
+    const partialsInjectFile = gulp.src(path.join(conf.paths.tmp, '/serve/app/templateCacheHtml.js'), { read: false });
+    const partialsInjectOptions = {
+        starttag: '<!-- inject:partials -->',
+        ignorePath: path.join(conf.paths.tmp, '/serve'),
+        addRootSlash: false
+    };
+
+    return gulp.src(path.join(conf.paths.tmp, '/serve/*.html'))
+        .pipe($.inject(partialsInjectFile, partialsInjectOptions))
+        .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve')));
+});
+
+gulp.task('clean', () => {
     return del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['inject']);
+gulp.task('build', ['html']);
