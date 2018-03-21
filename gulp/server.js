7e195316560cc5bdbbd354ce3a24fe379c90d959
yehuiwei <yehuiwei@hengtiansoft.com>
2018-03-21 17:08:13 +0800

    add cors support

--- gulp/server.js
+++ gulp/server.js
@@ -5,12 +5,19 @@ const path = require('path');
 const browserSync = require('browser-sync');
 const browserSyncSpa = require('browser-sync-spa');
 
+const proxyMiddleware = require('http-proxy-middleware');
+
 gulp.task('serve', ['build', 'watch'], function () {
     browserSync.init({
         server: {
             staticPath: '/',
             baseDir: path.join(conf.paths.tmp, '/serve'),
-            browser: "google chrome"
+            browser: "google chrome",
+            middleware: [proxyMiddleware('/v2', {
+                target: 'https://api.douban.com',
+                changeOrigin: true,
+                logLevel: 'debug'
+            })]
         }
     })
 
